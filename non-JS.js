78c9c3d0f58c8ab03dd51a40fd4a49cb80b4c8fc
ksm0520 <118537605+ksm0520@users.noreply.github.com>
2023-06-07 19:45:29 +0900

    Delete non-JS.js

--- non-JS.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const app = express()
-
-const nunjucks = require('nunjucks')
-
-app.set('view engine', 'html')
-nunjucks.configure('views',{
-    express:app,
-})
